311cf2407b356254c945a8a2eaaad97a7523549a
Samaeeeel <asrovalino@puce.edu.ec>
2025-01-14 08:42:33 -0500

    Alam2

--- especificaciones.js
+++ especificaciones.js
@@ -66,7 +66,7 @@ document.getElementById("specifications-form").addEventListener("submit", async 
     };
 
     try {
-        const response = await fetch("http://localhost:5000/guardar-especificaciones", {
+        const response = await fetch("zapateria-production.up.railway.app/guardar-especificaciones", {
             method: "POST",
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify(formData),
